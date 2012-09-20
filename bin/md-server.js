ac1cc1eba78b72f714ce732279d23bcfd3854eac
Puraumu <puraumu@macpro.local>
2012-09-20 22:55:40 +0900

    basic bin commands

--- bin/md-server.js
+++ bin/md-server.js
@@ -4,9 +4,36 @@
  * Module dependencies.
  */
 
-var commander = require('commander')
+var program = require('commander')
   , connect = require('connect')
   , custom = require('../')
+  , fs = require('fs')
   , app = connect()
 
+program
+  .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version)
+  .option('-p --port [8080]', 'Port to use (default :8080).', 8080)
+  .option('-s --silent', 'Do not show log messages.')
+
+program.name = 'md-server';
+
+program.parse(process.argv);
+
+if (program.port) {
+  var port = parseInt(program.port, 10);
+};
+
+if (!program.silent) {
+  app.use(connect.logger('dev'));
+};
+
+var root = program.args[0]
+
+app.use(custom.markdown(root));
+app.use(connect.static(root));
+app.use(connect.directory(root));
+
+app.listen(port);
+
+console.log('Serving %s on port: %d', root, port);
 
