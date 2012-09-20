33b1488fb827b943dfc697f1fb73e51d43f8b892
Puraumu <puraumu@macpro.local>
2012-09-20 22:19:26 +0900

    add markdwon middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var connect = require('connect')
+  , custom = require('./')
   , app = connect()
 
 app.use(connect.logger('dev'));
+app.use(custom.markdown('public'));
 app.use(connect.static('public'));
 app.use(connect.directory('public'));
 
