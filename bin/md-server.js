1e62180c7046892a7ef0edd64db132ea43fb8e11
Puraumu <puraumu@macpro.local>
2012-09-20 22:38:16 +0900

    add bin/

---
+++ bin/md-server.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var commander = require('commander')
+  , connect = require('connect')
+  , custom = require('../')
+  , app = connect()
+
+
