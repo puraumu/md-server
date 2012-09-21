d46103e8f2f2974fece50e5fff52af5723ab0c41
Puraumu <puraumu@macpro.local>
2012-09-21 22:06:00 +0900

    add toc support

--- bin/md-server.js
+++ bin/md-server.js
@@ -14,10 +14,13 @@ program
   .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json', 'utf8')).version)
   .option('-p --port [8080]', 'Port to use (default :8080).', 8080)
   .option('-s --silent', 'Do not show log messages.')
+  .option('-t --toc', 'Add Table of Contents to html.')
 
 program.name = 'md-server';
 
 program.parse(process.argv);
+
+var root = program.args[0];
 
 if (program.port) {
   var port = parseInt(program.port, 10);
@@ -27,9 +30,12 @@ if (!program.silent) {
   app.use(connect.logger('dev'));
 };
 
-var root = program.args[0]
+if (program.toc) {
+	app.use(custom.markdown(root, true));
+} else {
+	app.use(custom.markdown(root));
+};
 
-app.use(custom.markdown(root));
 app.use(connect.static(root));
 app.use(connect.directory(root));
 
